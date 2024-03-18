a605e6e9169deb4b0fbfa8e0e0575578b73ebcab
Raj Chowdhury <rchowdhury702@gmail.com>
2024-03-18 18:35:34 +0530

    Login Page and Register Page

---
+++ public/login.js
@@ -0,0 +1,56 @@
+// Show/hide password onClick of button using Javascript only
+
+// https://stackoverflow.com/questions/31224651/show-hide-password-onclick-of-button-using-javascript-only
+
+function LoginClicked() {
+    event.preventDefault();
+    // Get form data
+    const UID = document.getElementById("uniqueId").value;
+    const pwd = document.getElementById("password").value;
+    var jsonObject = {
+        uniqueId : UID,
+        password : pwd
+    }
+    console.log(JSON.stringify(jsonObject))
+    
+    document.getElementById("uniqueId").value = "";
+    document.getElementById("password").value = "";
+
+    // Send POST request to API
+    fetch('api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+             'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jsonObject)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Response from server:', data);
+        if(data.designation === "student")
+        window.location.href = "users/student.html";
+        else if(data.designation === "hod")
+        window.location.href = "users/hod.html";
+        else if(data.designation === "teacher")
+        window.location.href = "users/teacher.html";
+        else if(data.designation === "admin")
+        window.location.href = "users/admin.html";
+        else
+        window.location.href = "error.html";
+    })
+    .catch(error => {
+        console.log('Error:', error);
+    });
+}
+
+function goToRegister()
+{
+    event.preventDefault();
+    window.location.href = "register.html"
+}
+
---
+++ public/register.js
@@ -0,0 +1,42 @@
+function RegisterClicked()
+{
+    event.preventDefault();
+
+    var UID = document.getElementById("uniqueId").value;
+    var pwd = document.getElementById("password").value;
+    var mail = document.getElementById("email").value;
+    var name = document.getElementById("name").value;
+
+    const jsonObject = {
+        name : name,
+        uniqueId : UID,
+        email : mail,
+        password : pwd
+    }
+    
+    console.log(JSON.stringify(jsonObject))
+
+    fetch('api/v1/auth/register', {
+        method: 'PATCH',
+        headers: {
+             'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jsonObject)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Response from server:', data);
+        if(data.success === true)
+        window.location.href = "login.html";
+        else
+        window.location.href = "error.html";
+    })
+    .catch(error => {
+        console.log('Error:', error);
+    });
+}
