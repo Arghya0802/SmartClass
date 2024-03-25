b51d5c05f9cc008588d33643a3d9f0f341ea9942
Arghya0802 <chatterjeearghya02@gmail.com>
2024-03-25 11:15:58 +0530

    Added Verify Route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,18 @@
 import express from "express";
 const router = express.Router();
 
-import { login, logout, register } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  register,
+  verifyToken,
+} from "../controllers/auth.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 router.patch("/register", register);
 router.post("/login", login);
 router.get("/logout", verifyJWT, logout);
+
+router.get("/verify", verifyToken);
 
 export default router;
